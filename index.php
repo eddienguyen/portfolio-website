efedb4163cf0fa5daa9ded062478cca422610643
eddienguyen <eddienguyen996@gmail.com>
2018-04-19 12:56:30 +0700

    fisrt commit

---
+++ index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Eddie Nguyen 's portfolio</title>
+	<link rel="stylesheet" type="text/css" href="styles/style.css">
+</head>
+<body>
+
+	<div class="wrapper">
+		<div class="outer-image">
+			<img class="round-image" src="images/avatar.png" height="100%">
+		</div>
+
+		<div class="header"></div>
+	</div>
+
+	<div id="centerpixel">
+		<div id="portfolio">
+			<div class="inner">
+				<div class="portfolio_title">
+					NGUYEN HOANG QUAN <br>
+					<p>Personal Infomation</p>
+				</div>
+
+				<table class="portfolio_content">
+					<tr>
+						<td>Age</td>
+						<td class="one">:</td>
+						<td>22</td>
+					</tr>
+					<tr>
+						<td>Gradutation</td>
+						<td class="one">:</td>
+						<td>Hanoi University of Bussiness and Technology</td>
+					</tr>
+					<tr>
+						<td>Skills</td>
+						<td class="one">:</td>
+						<td>Photoshop, HTML, CSS, Java, MySQL</td>
+					</tr>
+					<tr>
+						<td>Experience with</td>
+						<td class="one">:</td>
+						<td>SCRUM, Agile</td>
+					</tr>
+					<tr>
+						<td>Strength</td>
+						<td class="one">:</td>
+						<td>Hardwork</td>
+					</tr>
+					<tr>
+						<td>Weakness</td>
+						<td class="one">:</td>
+						<td>Teamwork</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	</div>
+
+	<div id="LineTime">
+		<div class="container-fluid">
+			<div class="row timeline-centered">
+				<div class="col-md-12 timelineMAIN-title">
+					<ul class="timeline timeline-centered">
+						<li class="timeline-item">
+							<div class="timeline-info">
+								<span>2017</span>
+							</div>
+							<div class="timeline-marker"></div>
+							<div class="timeline-content">
+								<h3>Code Intensive at Techkids</h3>
+								<p>created Smith-it Smith - a platformer side-scrolling game. Written in Java. Achieved SCRUM project management, Java OOP,...</p>
+								<a href="https://github.com/eddienguyen/SiS-alpha-" target="_blank"> source code and product.</a>
+							</div>
+						</li>
+
+						<li class="timeline-item">
+							<div class="timeline-info">
+								<span>2016</span>
+							</div>
+							<div class="timeline-marker"></div>
+							<div class="timeline-content">
+								<h3>Photoshop</h3>
+								<p>"Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Donec vitae sapien ut libero venenatis faucibus. ullam dictum felis
+                                eu pede mollis pretium. Pellentesque ut neque."</p>
+							</div>
+						</li>
+
+						<li class="timeline-item">
+							<div class="timeline-info">
+								<span>2015</span>
+							</div>
+							<div class="timeline-marker"></div>
+							<div class="timeline-content">
+								<h3>Lorem ipsum</h3>
+								<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco..."</p>
+							</div>
+						</li>
+
+
+					</ul>
+				</div>
+				
+			</div>
+		</div>
+
+		<div id="wraptheform">
+			<div id="contactform">
+				<h4>Thanks for reading!</h4>
+			</div>
+			<div class="contactwas">
+				<center><h4>
+					Contact me through e-mail. I will stay in touch.
+				</h4></center>
+			</div>
+
+			<form method="post" id="mailer">
+				
+				<p class="name">
+            		<input name="name" type="text" placeholder="Name" />
+        		</p>
+
+				<p class="email">
+					<input type="text" name="email" placeholder="Email">
+				</p>
+        		
+        		<p class="text">
+					<textarea name="message" placeholder="Comment"></textarea>
+				</p>
+        		
+        		<div class="reset">
+					<input type="reset" value="RESET">
+        		</div>
+
+        		<div class="submit">
+        			<input type="submit" value="SEND">
+        		</div>
+
+			</form>
+		</div>
+
+		<div class="footer"><center> Copyright 2018 @Eddienguyen996</center></div>
+	</div>
+
+
+
+</body>
+</html>
